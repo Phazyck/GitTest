463e0f4a930bc6859f368460da9fc1d2b490b313
Oliver Phillip Roer <phazyck@gmail.com>
2013-02-26 20:43:32 +0100

    Added dialog and command line arguments.

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -10,16 +10,28 @@ namespace GitTest
     {
         static void Main(string[] args)
         {
-            int n = 10;
-            string greeting = String.Format("Hi there! I'm going to print the first {0} fibonacci numbers!", n);
+            int n;
+            try
+            {
+                n = Int32.Parse(args[0]);
+            }
+            catch (Exception e)
+            {
+                n = 15;
+            }
+
+            string greeting = String.Format("Hi there! I'm going to print the first {0} fibonacci numbers!\n", n);
             Console.WriteLine(greeting);
+            Console.WriteLine("When you're ready for it, press any key. ^_^\n");
+            Console.ReadKey();
             int[] fs = fib(n);
             for (int i = 0; i < n; i++)
             {
-                string f = String.Format("{0,2:d}:{1,3:d}", i+1, fs[i]);
+                string f = String.Format("{0,3:d}:{1,4:d}", i+1, fs[i]);
                 Console.WriteLine(f);
             }
-            Console.Read();
+            Console.WriteLine("\n*Whew* that was all of them.\nPress any key to quit.");
+            Console.ReadKey();
         }
 
         public static int[] fib(int n)
